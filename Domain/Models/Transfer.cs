782b10a38f01e89cc727dfac1ead5d44c967412a
DiegoJoelRolon <diegorolon01@outlook.com>
2024-09-25 23:03:23 -0300

    Repo Transfer

---
+++ Domain/Models/Transfer.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Models
+{
+    public class Transfer
+    {
+        public Guid Id { get; set; }
+        public required decimal Amount { get; set; }
+        public required DateTime Date { get; set; }
+        public required string Status { get; set; } //podriamos tener un objeto de tipo Status?
+        public required string Description { get; set; }
+
+        public required int TypeId { get; set; }
+        public TransferType TransferType { get; set; }
+
+        public required Guid SrcAccountId { get; set; }
+        public required Guid DestAccountId { get; set; }
+    }
+}
---
+++ Domain/Models/TransferType.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models
+{
+    public class TransferType
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
